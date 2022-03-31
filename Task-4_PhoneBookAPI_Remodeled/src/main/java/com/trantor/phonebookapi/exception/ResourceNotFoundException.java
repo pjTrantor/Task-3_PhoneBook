7aabe4d5a75ed040724300637815206a22c08e32
pjTrantor <102573015+pjTrantor@users.noreply.github.com>
2022-03-31 18:13:30 +0530

    Delete Task-4_PhoneBookAPI_Remodeled directory

--- Task-4_PhoneBookAPI_Remodeled/src/main/java/com/trantor/phonebookapi/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.trantor.phonebookapi.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
